406f79904c967e78a61a6d8071bb52c0cfb6e1e5
Gregg Hosie <gregg.hosie@gmail.com>
2018-12-17 12:25:31 -0500

    JS updated, win/loss incrementor fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-
+  // Set global variables
   // list of words
   var wordList = ["sol", "mercury", "venus", "earth", "luna", "mars", "jupiter", "saturn", "uranus", "neptune"];
   // wins losses and "lives" counters
@@ -10,9 +10,11 @@ window.onload = function () {
   var correctLetters = 0;
   var correctLettersList = [];
   var wrongLettersList = [];
+  var blankAnswer = [];
+
+  gameActive = true;
 
   // variables to hold references for display
-
   var alertTextElement = document.getElementById("alert-text");
   var wordBlanksElement = document.getElementById("word-Blanks");
   var winTallyElement = document.getElementById("winTally-Text");
@@ -20,33 +22,28 @@ window.onload = function () {
   var triesLeftElement = document.getElementById("livesLeft-Text");
   var wrongLettersListElement = document.getElementById("wrong-Guesses");
 
-
-
   // generates random word from word list
   var word = wordList[Math.floor(Math.random() * wordList.length)];
   console.log(word);
   // sets up empty answer array
-  var blankAnswer = [];
+
   for (var i = 0; i < word.length; i++) {
     blankAnswer[i] = "_";
   }
 
   // grab keypress onkeyup and set to letterGuess
   document.onkeyup = function (event) {
+    // checks both wrong and correct letter lists for captured input
     if (wrongLettersList.indexOf(event.key) === -1 && correctLettersList.indexOf(event.key) === -1) {
-      console.log(event.which);
       if (wrongLettersList)
         if (event.which >= 65 && event.which <= 90) {
-          // user input is a letter
+          // user input is a letter a - z
           var letterGuess = event.key;
-          // sets letters left to length of chosen word
-
-
           // boolean for tries/lives counter
           var foundCorrectLetter = false;
 
+          // generates blank spaces for unguess word letters
           for (var i = 0; i < word.length; i++) {
-
             if (word.charAt(i) === letterGuess.toLowerCase()) {
               foundCorrectLetter = true;
               blankAnswer[i] = letterGuess;
@@ -55,7 +52,7 @@ window.onload = function () {
             wordBlanksElement.textContent = blankAnswer.join(" ");
           }
 
-
+          // if game is not lost, minus tries counter, add  incorrect keypress to wrongLetterList - and correct keypress to correctLetterList
           if (foundCorrectLetter === false && tries > 0) {
             tries--;
             if (wrongLettersList.indexOf(event.key) === -1) {
@@ -67,11 +64,15 @@ window.onload = function () {
 
           wrongLettersListElement.textContent = wrongLettersList.join(", ");
           triesLeftElement.textContent = "You have " + tries + " tries left.";
+          // if user runs out of tries, game is lost
           if (tries <= 0) {
             alertTextElement.textContent = "You have lost! Please press enter to try again.";
+            losses++;
+            lossTallyElement.textContent = "You have lost " + losses + " times.";
+            gameActive = false;
           }
         } else {
-          // user input is not a letter
+          // if tries above 0 or word guessed - the enter key resets game
           if (tries <= 0 || correctLetters >= word.length && event.which === 13) {
             initializeNewWord();
           }
@@ -79,6 +80,10 @@ window.onload = function () {
         };
       if (correctLetters === word.length) {
         alertTextElement.textContent = "You've Won! Please press enter to try again."
+        wins++;
+        winTallyElement.textContent = "You have won " + wins + " times."
+        gameActive = false;
+
       }
 
     }
@@ -89,23 +94,23 @@ window.onload = function () {
     correctLetters = 0;
     word = wordList[Math.floor(Math.random() * wordList.length)];
     blankAnswer = [];
-    wrongLettersList = [];
-    correctLettersList = [];
     for (var i = 0; i < word.length; i++) {
       blankAnswer[i] = "_";
       alertTextElement.textContent = "Press any key to get started!";
     }
+    wrongLettersList = [];
+    correctLettersList = [];
     triesLeftElement.textContent = "You have " + tries + " tries left.";
     wordBlanksElement.textContent = blankAnswer.join(" ");
     wrongLettersListElement.textContent = wrongLettersList.join(", ");
+    
+    
   }
 
   winTallyElement.textContent = "Your score is " + wins + ".";
   lossTallyElement.textContent = "You have lost " + losses + " times.";
   triesLeftElement.textContent = "You have " + tries + " tries left.";
   wordBlanksElement.textContent = blankAnswer.join(" ");
-  console.log(blankAnswer.join(" "));
-  // wordBlanks.textContent = blankAnswer;
   wrongLettersListElement.textContent = wrongLettersList.join(", ");
 
 }
